51e2f58b07455d10bfbda9af4f649754ed4138b6
SamEBae <baesammie@gmail.com>
2015-03-02 17:18:49 -0500

    Teams update

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// function explore(form){
+// 	//document.getElementById(form).innerHTML = "VIEW GALLERY";
+// 	//$('#'+form).text("VIEW GALLERY").fadeIn(1000);
+// 	function(){
+//        $('#'+form).fadeOut("slow", function(){
+//                 $('#'+form).html("Good Bye")
+//             }).fadeIn("slow");
+// }
+// $("#explore").hover(
+//    function() {
+//        $(this).fadeIn(2500,function(){ 
+//                 $(this).html("View GALLERY")
+//             });
+// });
