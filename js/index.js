aac31e95fc78329423da3c4e5afcca2802293c45
SamEBae <baesammie@gmail.com>
2015-03-29 21:21:52 -0400

    better ios (smaller screen) support. fixed logo positioning

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
-$( window ).scroll(function() {
+var firstAnimation = false;
+$(window).scroll(function() {
 	var height = $(window).scrollTop();
 	
 	if($(window).width() > 739) {     
-  	//Add your javascript for large screens here
+  	//for nav bar changing from tranparent to grey
   		if(height>=900){
 			$( ".navbar-default" ).css( "background-color", "#DBDAD9" );
 			$( ".navbar-fixed-top *" ).css( "background-color", "#DBDAD9" );
@@ -11,10 +12,44 @@ $( window ).scroll(function() {
 			$( ".navbar-default" ).css( "background-color", "transparent" );
 			$( ".navbar-fixed-top *" ).css( "background-color", "transparent" );
 		}
+
+	//for animating icons float
+		if(height>=1000&&!firstAnimation){
+			firstAnimation = true;
+
+			// $("#pencil").css({
+			// 	 '-webkit-transform': 'translateX(-20px)',
+   //               'transform': 'translateX(-20px)',
+   //               '-webkit-transition-duration': '0.5s',
+ 		// 		 'transition-duration': '0.5s'
+			// })
+		}
+		// setTimeout(function(){
+		// 	$("#pencil").removeAttr('style');
+		// }, 2000);
+
+		// if(height>=1000){
+		// 	$("#bubbles").css({
+		// 		 '-webkit-transform': 'translateY(-20px)',
+  //                'transform': 'translateY(-20px)'
+		// 	})
+		// }
+		// setTimeout(function(){
+		// 	$("#bubbles").removeAttr('style');
+		// }, 2000);
+
+		// if(height>=1000){
+		// 	$("#space").css({
+		// 		 '-webkit-transform': 'translateY(-20px)',
+  //                'transform': 'translateY(-20px)'
+		// 	})
+		// }
+		// setTimeout(function(){
+		// 	$("#space").removeAttr('style');
+		// }, 2000);
 	}
 	if($(window).width() < 739) {
-		//alert("z");
-	  //Add your javascript for small screens here
+	//for nav bar changing from tranparent to grey
 	  	if(height>=200){
 	  		$( ".navbar-default" ).css( "background-color", "#DBDAD9" );
 			$( ".navbar-fixed-top *" ).css( "background-color", "#DBDAD9" );
@@ -22,6 +57,9 @@ $( window ).scroll(function() {
 			$( ".navbar-default" ).css( "background-color", "transparent" );
 			$( ".navbar-fixed-top *" ).css( "background-color", "transparent" );	
 		}
+
 	}
 	console.log(height);
 });
+
+
