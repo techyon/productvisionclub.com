7c7cc179cf6160dffa3bf74f6700784c4aaedf43
Sammy Bae <sammybae@I003916.local>
2015-09-30 17:45:28 -0600

    minor style and content changes

--- js/app.js
+++ js/app.js
@@ -10,17 +10,17 @@
     
     {name:'Kartik Talwar', imageURL:"images/team/kartik.jpg",role:"Advisor",program:"4A Physics", email:"", LinkedIn:"https://www.linkedin.com/in/kartiktalwar", description:"Co-Founder of Hack the North and Waterloo API developer as well as various internships in the valley, Kartik is an experienced developer and has track record of getting stuff done"},
     
-    {name:'Kaivalya Gandhi', imageURL:"images/team/kaivalya.jpg",role:"Co-President",program:"2B Software Engineering", email:"kaivalya.gandhi@gmail.com", LinkedIn:"https://www.linkedin.com/in/kaivalyagandhi"},
+    {name:'Kaivalya Gandhi', imageURL:"images/team/kaivalya.jpg",role:"Co-President",program:"3A Software Engineering", email:"kaivalya.gandhi@gmail.com", LinkedIn:"https://www.linkedin.com/in/kaivalyagandhi"},
     
     {name:'Helen Huang', imageURL:"images/team/helen.jpg",role:"Co-President",program:"3B Science & Business", email:"helenlyhuang@gmail.com", LinkedIn:"https://ca.linkedin.com/in/helenlyhuang", 
     description:"Aside from my actual responsibilities and club goals in wanting to grow the PVC presence and credibility, I enjoy playing computer games, dancing, and being lazy in general"},
     
     {name:'Sammie Bae', imageURL:"images/team/sammie.jpg",role:"Technology Co-Director",program:"2A System Design Engineering", email:"baesammie@gmail.com", LinkedIn:"https://ca.linkedin.com/in/sammiebae", 
-    description:"sammiebae.me"},
+    description:"sammiebae.com"},
     
     {name:'Reginald Suh', imageURL:"images/team/regi.jpg",role:"Technology Co-Director",program:"2A Computer Science", email:"rsuh@uwaterloo.ca", LinkedIn:"https://ca.linkedin.com/in/reginaldsuh"},
     
-    {name:'Simran Jassal', imageURL:"images/team/simran.jpg",role:"CO-DIRECTOR OF PRODUCT VISION CHALLENGE & TECH",program:"1B System Design Engineering", email:"simran.jassal@hotmail.com", LinkedIn:"https://ca.linkedin.com/in/simranjassal"},
+    {name:'Simran Jassal', imageURL:"images/team/simran.jpg",role:"CO-DIRECTOR OF PRODUCT VISION CHALLENGE & TECH",program:"2A System Design Engineering", email:"simran.jassal@hotmail.com", LinkedIn:"https://ca.linkedin.com/in/simranjassal"},
     
     {name:'Pedja Ristic', imageURL:"images/team/pedja.jpg",role:"MEDIA DIRECTOR",program:"Global Business & Digital Arts", email:"peda.ristic@live.ca", LinkedIn:"https://ca.linkedin.com/in/pedjaristic", 
     description:"A Ballroom dancing tech fiend with a fetish for programming and design. Pedja’s aim is to paint the walls of user interaction with the colour of quality."}
@@ -43,16 +43,14 @@
       {imageURL:"images/Gallery/9.jpg", header: "Interview", subheader: "Information Sessions",link:"projects/project-4.html"},
       {imageURL:"images/Gallery/15.jpg", header: "BlackBerry", subheader: "Innovation Trek",link:"projects/project-6.html"},
       {imageURL:"images/Gallery/16.jpg", header: "BlackBerry", subheader: "Innovation Trek",link:"projects/project-6.html"},
-
+      {imageURL:"images/Gallery/17.jpg", header: "Student PMs", subheader: "Information Sessions"},
+      {imageURL:"images/Gallery/18.jpg", header: "Student PMs", subheader: "Information Sessions"}
     ];
 
     $scope.portfoliosClass = function (index){
       //console.log($scope.portfolios[index].subheader);
-
       if($scope.portfolios[index].subheader=="Information Sessions") return "sessions";
       if($scope.portfolios[index].subheader=="Tech Talks") return "talk";
       if($scope.portfolios[index].subheader=="Innovation Trek") return "trek";
-      console.log($scope.portfolios[index].subheader);
-      
     }
   }]);
